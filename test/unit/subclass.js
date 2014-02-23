4821e1ff251bf6a5cc1694a69d1f5c20a19190f2
cyberfunk <hello@cyberfunk.org>
2014-02-24 01:03:58 +0800

    Update tests.

--- test/unit/subclass.js
+++ test/unit/subclass.js
@@ -5,34 +5,6 @@ var Interface = interface.Interface;
 var cli = require('cli-command');
 var CommandProgram = cli.CommandProgram;
 
-var SubClass = function() {
-  Interface.apply(this, arguments);
-}
-
-util.inherits(SubClass, Interface);
-
-SubClass.prototype.configure = function() {
-  expect(this.configure).to.be.a('function');
-  expect(this.configure({})).to.equal(this);
-}
-
-SubClass.prototype.use = function() {
-  expect(this.use).to.be.a('function');
-  expect(this.use()).to.equal(this);
-}
-
-SubClass.prototype.command = function() {
-  expect(this.command).to.be.a('function');
-  expect(this.command('mock', 'mock command')).to.equal(this);
-}
-
-SubClass.prototype.option = function() {
-  expect(this.option).to.be.a('function');
-  expect(this.option('--mock-flag',
-    'mock flag option')).to.equal(this);
-  expect(this.option('--mock-argument=[value]',
-    'mock flag argument')).to.equal(this);
-}
 
 describe('cli-interface:', function() {
   it('should create interface (module)', function(done) {
@@ -48,6 +20,42 @@ describe('cli-interface:', function() {
     done();
   });
   it('should create subclass', function(done) {
+    var SubClass = function() {
+      Interface.apply(this, arguments);
+    }
+    util.inherits(SubClass, Interface);
+
+    SubClass.prototype.configure = function() {
+      expect(this.configure).to.be.a('function');
+      expect(this.configure({})).to.equal(this);
+    }
+
+    SubClass.prototype.use = function() {
+      expect(this.use).to.be.a('function');
+      expect(this.use()).to.equal(this);
+    }
+
+    SubClass.prototype.command = function() {
+      expect(this.command).to.be.a('function');
+      expect(this.command('mock', 'mock command')).to.equal(this);
+    }
+
+    SubClass.prototype.option = function() {
+      expect(this.option).to.be.a('function');
+      expect(this.option('--mock-flag',
+        'mock flag option')).to.equal(this);
+      expect(this.option('--mock-argument=[value]',
+        'mock flag argument')).to.equal(this);
+    }
+
+    SubClass.prototype.on = function() {
+      this.on('complete', function(req) {
+        expect(this.mockFlag).to.eql(true);
+        expect(this.mockArgument).to.eql('value');
+        done();
+      })
+    }
+
     var prg = new SubClass(null, 'mock-subclass-instance');
     expect(prg).to.be.instanceof(Interface);
     expect(prg).to.be.instanceof(SubClass);
@@ -57,8 +65,5 @@ describe('cli-interface:', function() {
     expect(prg.program._options.mockArgument).to.be.an('object');
     var args = ['--mock-flag', '--mock-argument=value'];
     prg.parse(args);
-    expect(prg.program.mockFlag).to.eql(true);
-    expect(prg.program.mockArgument).to.eql('value');
-    done();
   });
 });
